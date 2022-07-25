c33deabb7a931648a9466222e0c7b6eab5cd8052
Samhitha Veldi <samhitha.veldi@zemosolabs.com>
2022-07-25 13:09:14 +0530

    first commit

---
+++ e2e/firstTest.e2e.ts
@@ -0,0 +1,16 @@
+
+describe('Example', () => {
+  beforeAll(async () => {
+    await device.launchApp();
+  });
+
+  beforeEach(async () => {
+    await device.reloadReactNative();
+  });
+
+  it('should have welcome screen', async () => {
+    await expect(element(by.id('login_screen'))).toBeVisible();
+  });
+
+
+});
